5a52cc9f624867fd96be40edbfd17acdda92a98f
Nuno Miguel Fernandes <55770319+nmfer@users.noreply.github.com>
2021-01-07 08:36:00 +0000

    Update agente.py

--- agente.py
+++ agente.py
@@ -435,8 +435,8 @@ def Media_Tempo_Deslocamento(tempo):
 	#posição das escadas
 	#if (X == 180) and (30 <= Y and Y <= 45):
 	#	posicao_escadas = 'Escadas'
-	distancia = math.sqrt((100 - X_ant)**2 + (100 - Y_ant)**2)
-	tempo_des = tempo - a
+	distancia = math.sqrt((0 - X_ant)**2 + (0 - Y_ant)**2)
+	tempo_des = tempo - aa
 
 	media =  tempo_des / distancia
 
