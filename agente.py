8cfb9c7fecdd958b8a5403047b8fdecca437c923
Nuno Miguel Fernandes <55770319+nmfer@users.noreply.github.com>
2021-01-07 07:39:54 +0000

    Update agente.py

--- agente.py
+++ agente.py
@@ -342,9 +342,9 @@ def Distancia_sala_Enfermeiro():
 	#se só existe 1 sala de enfermeiros
 	if count == 1:
 		X_sala = sala_aux[1]
-		#print(X_sala)
+		print(X_sala)
 		Y_sala = sala_aux[2]
-		#print(Y_sala)
+		print(Y_sala)
 		#calcula a distância da posição em que o dito se encontra até à única sala existente
 		distancia = math.sqrt((X_sala - X)**2 + (Y_sala - Y)**2)
 
